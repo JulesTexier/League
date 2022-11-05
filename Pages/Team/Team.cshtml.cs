f8425971c6d88e73e4757e26517f1d1b8cb4be2b
Tex <julestexier@live.fr>
2022-11-05 14:24:50 +0100

    création des pages Team

--- Pages/Team/Team.cshtml.cs
+++ Pages/Team/Team.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using League.Data;
+using League.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace League.Pages.Team
+namespace LeagueTeam
 {
     public class TeamModel : PageModel
     {
-        public void OnGet()
+        private readonly LeagueContext _context;
+        public Team team { get; set; }
+
+        public TeamModel (LeagueContext context)
         {
+            _context = context;
+
+        }
+
+
+        public async Task OnGetAsync(string id)
+        {
+            team = await _context.Teams.FindAsync(id);
         }
     }
 }
