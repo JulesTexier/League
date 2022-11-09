08044711fba1e82a8b22a4b153e540f37cf94b77
Tex <julestexier@live.fr>
2022-11-09 22:55:21 +0100

    page player

--- Pages/Players/Index.cshtml.cs
+++ Pages/Players/Index.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using League.Data;
+using League.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace PlayersIndex
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+
+        public List <Player> Players { get; set; }
+        private LeagueContext _context;
+
+        public IndexModel(LeagueContext context)
         {
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Players = await _context.Players.ToListAsync();
         }
     }
 }
---
+++ Pages/Players/Player.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+using League.Models;
+using League.Data;
+
+namespace LeaguePlayers
+{
+    public class PlayerModel : PageModel
+    {
+        public readonly LeagueContext _context;
+        public Player player;
+
+        public PlayerModel(LeagueContext context)
+        {
+           _context = context;
+        }
+
+        public async Task OnGetAsync(string id)
+        {
+            player = await _context.Players.FindAsync(id);
+        }
+    }
+}
